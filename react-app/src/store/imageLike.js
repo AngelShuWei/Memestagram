c35f4bc0ba941a610f659a415855b2faf3c09ff7
Leah Kim <lk.leahkim@gmail.com>
2022-04-04 14:00:08 -0700

    working on likes thunks

--- react-app/src/store/imageLike.js
+++ react-app/src/store/imageLike.js
@@ -1,41 +1,62 @@
-const LOAD_LIKE = 'like/LOAD_LIKE';
-const CREATE_LIKE = 'like/CREATE_LIKE';
-const DELETE_LIKE = 'like/DELETE_LIKE';
+const LOAD_IMG_LIKE = 'like/LOAD_LIKE';
+const CREATE_IMG_LIKE = 'like/CREATE_LIKE';
+const DELETE_IMG_LIKE = 'like/DELETE_LIKE';
 
 
-const loadLike = (like) => ({
-    type: LOAD_LIKE,
+const loadImgLike = (like) => ({
+    type: LOAD_IMG_LIKE,
     like
 })
 
-const createLike = (like) => ({
-    type: CREATE_LIKE,
+const createImgLike = (like) => ({
+    type: CREATE_IMG_LIKE,
     like
 })
 
-const deleteLike = (id) => ({
-    type: DELETE_LIKE,
+const deleteImgLike = (id) => ({
+    type: DELETE_IMG_LIKE,
     id
 })
 
-export const allLikes = () => async (dispatch) => {
+export const allImgLikes = () => async (dispatch) => {
     const response = await fetch('/api/like');
 
     if (response.ok) {
         const data = await response.json();
-        await dispatch(loadLike(data))
+        await dispatch(loadImgLike(data))
     }
     return response
 }
 
-export const postLikes = (userId, ) => async(dispatch) => {
+export const postImgLikes = (userId, photoId) => async(dispatch) => {
     const response = await fetch(`/api/like/create/${userId}`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-
+            userId,
+            photoId
         })
     })
+
+    if (response.ok) {
+        const data = await response.json();
+        dispatch(createImgLike(data))
+    }
+    return response
 }
+
+export const deletingImgLike = (photoId) => async(dispatch) => {
+    const response = await fetch(`api/like/delete/${photoId}`, {
+        method: 'DELETE',
+    })
+    if (response.ok) {
+        const data = await response.json();
+        dispatch(deleteImgLike(data))
+    }
+}
+
+const initialState = {};
+
+const likesReducer = (state = initial)

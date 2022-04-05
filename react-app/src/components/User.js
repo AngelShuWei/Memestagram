96b73c17c448fb84741153dfdfcfd4da0a2d342d
angel <angel.wei101@gmail.com>
2022-04-05 00:00:03 -0700

    made user prof info appear

--- react-app/src/components/User.js
+++ react-app/src/components/User.js
@@ -10,6 +10,9 @@ function User() {
   const [user, setUser] = useState({});
   const { userId } = useParams();
 
+  // const users = useSelector(state => state.session)
+  // console.log(users);
+
   const userPosts = useSelector(state => Object.values(state.posts).filter(post => {
     //+ is cheat way for parseInt
     return post.user_id === +userId;
@@ -36,28 +39,15 @@ function User() {
 
   return (
     <>
-      <div className='page-container'>
+      <div className='prof-page-container'>
         <div className='prof-info-container'>
-          <ul className='ullinkfix'>
-            <li>
-              <strong>User Id</strong> {userId}
-            </li>
-            <li>
-              <strong>Username</strong> {user.username}
-            </li>
-            <li>
-              <strong></strong> {user.email}
-            </li>
-          </ul>
-          <p> IS THIS SHOWING</p>
-        <div>
-          <p>Testing</p>
-        </div>
+          <div className='prof-username'>{user.username}</div>
+          <div className='prof-name'>{user.name}</div>
         </div>
         <div className='prof-posts-img'>POSTS</div>
         <div className='prof-posts-container'>
           {userPosts.map(post =>
-            <div key={post.id}>
+            <div className='prof-post-container' key={post.id}>
               <NavLink to={`/post/${post.id}`}><img src={post?.image_url} alt="pic" style={{ width: "293px", height: "293px"}} /></NavLink>
             </div>
           )}

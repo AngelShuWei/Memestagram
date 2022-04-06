7716a4633d7d4d5e8bf8294264efcda12629cd4f
Leah Kim <lk.leahkim@gmail.com>
2022-04-05 20:24:54 -0700

    finished explore page styling

--- react-app/src/components/ExplorePage/ExplorePage.js
+++ react-app/src/components/ExplorePage/ExplorePage.js
@@ -7,10 +7,13 @@ function ExplorePage() {
     const userPosts = useSelector(state => Object.values(state.posts))
 
     return(
-        <div>
+        <div className='explore-page-container'>
+            <div className="explore-page-title">
+                
+            </div>
             {userPosts.map(post => (
-                <div>
-                    <img src={post.image_url}></img>
+                <div className="explore-pg-img-container" key={post.id}>
+                    <img className="explore-pg-img"src={post.image_url} alt="pic" style={{ width: "293px", height: "293px" }}></img>
                 </div>
             ))}
         </div>

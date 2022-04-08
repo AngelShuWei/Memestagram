64026c8b1eb6c17f2fd761c9d941ac44e576db0a
Leah Kim <lk.leahkim@gmail.com>
2022-04-08 16:36:58 -0700

    links work on prof pics

--- react-app/src/components/AboutPage/AboutPage.js
+++ react-app/src/components/AboutPage/AboutPage.js
@@ -1,18 +1,24 @@
 import './AboutPage.css';
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import yavuzPic from '../IconPics/sifu.jpeg';
 import angelPic from '../IconPics/disgusted.jpg';
 import leahPic from '../IconPics/images.jpg';
 import philipPic from '../IconPics/philip.jpg';
+import dogestagram from '../IconPics/dogestagram.png';
 
 function AboutPage() {
+
+    const handleYavuzClick = () => {
+      window.open("https://github.com/yavuzabasiyanik");
+    };
+
     return(
         <>
       <div className='prof-page-container'>
         <div className='prof-info-container'>
           <div className='prof-info-left'>
-            <img style={{ width: "150px", height: "150px", 'borderRadius': '100px' }}></img>
+            <img style={{ width: "150px", height: "150px", 'borderRadius': '100px' }} src={dogestagram}></img>
           </div>
           <div className='profile-info-right'>
             <div className='prof-name'>Memestagram</div>
@@ -25,20 +31,20 @@ function AboutPage() {
         <div className="friends-container">
 
             <div className="yavuz-div">
-                <img src={yavuzPic} style={{width:'293px', height:'293px'}}></img>
+                <a target="_blank"href='https://github.com/yavuzabasiyanik'><img src={yavuzPic} style={{width:'293px', height:'293px'}} ></img></a>
+
 
             </div>
             <div className="angel-div">
-                <img src={angelPic} style={{width:'293px', height:'293px'}}></img>
+                <a target="_blank"href='https://github.com/AngelShuWei'> <img src={angelPic} style={{width:'293px', height:'293px'}}></img></a>
 
             </div>
             <div className="leah-div">
-                <img src={leahPic} style={{width:'293px', height:'293px'}}></img>
+                <a target="_blank" href='https://github.com/Leahk1m'> <img src={leahPic} style={{width:'293px', height:'293px'}}></img></a>
 
             </div>
             <div className="philip-div">
-                <img src={philipPic} style={{width:'293px', height:'293px'}}></img>
-
+                <a target="_blank" href="https://github.com/philroberts13"><img src={philipPic} style={{width:'293px', height:'293px'}}></img></a>
             </div>
         </div>
 

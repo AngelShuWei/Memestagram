0e0b7e673ba43e207e30d3efd2629af0948598b4
Leah Kim <lk.leahkim@gmail.com>
2022-04-08 15:54:40 -0700

    added prof pics to about site creators

--- react-app/src/components/AboutPage/AboutPage.js
+++ react-app/src/components/AboutPage/AboutPage.js
@@ -1,11 +1,49 @@
 import './AboutPage.css';
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import yavuzPic from '../IconPics/sifu.jpeg';
+import angelPic from '../IconPics/disgusted.jpg';
+import leahPic from '../IconPics/images.jpg';
+import philipPic from '../IconPics/philip.jpg';
 
 function AboutPage() {
     return(
-        <div>
-            <h4>hello</h4>
+        <>
+      <div className='prof-page-container'>
+        <div className='prof-info-container'>
+          <div className='prof-info-left'>
+            <img style={{ width: "150px", height: "150px", 'borderRadius': '100px' }}></img>
+          </div>
+          <div className='profile-info-right'>
+            <div className='prof-name'>Memestagram</div>
+            <div className='prof-bio'>?</div>
+          </div>
         </div>
+        <div className='prof-posts-line'></div>
+        <div className='prof-posts-img'>POSTS</div>
+
+        <div className="friends-container">
+
+            <div className="yavuz-div">
+                <img src={yavuzPic} style={{width:'293px', height:'293px'}}></img>
+
+            </div>
+            <div className="angel-div">
+                <img src={angelPic} style={{width:'293px', height:'293px'}}></img>
+
+            </div>
+            <div className="leah-div">
+                <img src={leahPic} style={{width:'293px', height:'293px'}}></img>
+
+            </div>
+            <div className="philip-div">
+                <img src={philipPic} style={{width:'293px', height:'293px'}}></img>
+
+            </div>
+        </div>
+
+      </div>
+    </>
     );
 }
 

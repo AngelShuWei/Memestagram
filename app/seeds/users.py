c9559dc8643572f7cab30de1efb70e3c5ec4751c
angel <angel.wei101@gmail.com>
2024-01-11 18:16:19 -0800

    testing to see if can push into docker

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -31,7 +31,7 @@ def seed_users():
 # SQLAlchemy doesn't have a built in function to do this
 # TRUNCATE Removes all the data from the table, and RESET IDENTITY
 # resets the auto incrementing primary key, CASCADE deletes any
-# dependent entities
+# dependent entities.
 def undo_users():
     db.session.execute('TRUNCATE users RESTART IDENTITY CASCADE;')
     db.session.commit()
